77e300c823731132e12de8c09aa7ea51aedc1220
saikumar <saikumargurajalaa@gmail.com>
2026-05-15 21:07:23 +0530

    first commit

---
+++ functions/parameter.py
@@ -0,0 +1,14 @@
+#function with parameter with return value
+
+def add(a,b):
+    return a+b
+print(add(10,2))
+s=add(11,12)
+print(s)
+
+
+#functions without parameter with return value
+
+def sentence():
+    return "hello world"
+print(sentence())
