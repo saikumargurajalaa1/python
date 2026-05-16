29ab60484e57efd7678a29509bb0a22273f6f7a3
saikumar <saikumargurajalaa@gmail.com>
2026-05-16 13:07:04 +0530

    Updated files

--- functions/calling one in another.py
+++ functions/calling one in another.py
@@ -5,10 +5,11 @@
 #     print("banana()")
 # sai()
 
-def add(a,b):
-    print("sum is",a+b)
-    print(mul(a,b))
-def mul(a,b):
-    return(a*b)
-add(10,2)
-print("xxxx")
+#
+# def add(a,b):
+#     print("sum is",a+b)
+#     print(mul(a,b))
+# def mul(a,b):
+#     return(a*b)
+# add(10,2)
+# print("xxxx")
