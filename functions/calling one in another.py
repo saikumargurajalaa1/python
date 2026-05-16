d501e0a508023ee0e8253ec2064225d07fb58bc6
saikumar <saikumargurajalaa@gmail.com>
2026-05-16 11:50:13 +0530

    message

---
+++ functions/calling one in another.py
@@ -0,0 +1,14 @@
+# def sai():
+#     print("apple()")
+#     kumar()
+# def kumar():
+#     print("banana()")
+# sai()
+
+def add(a,b):
+    print("sum is",a+b)
+    print(mul(a,b))
+def mul(a,b):
+    return(a*b)
+add(10,2)
+print("xxxx")
