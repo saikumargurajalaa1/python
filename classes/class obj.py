03804592ae6785eb8bca0aff700ed4f330527b3c
saikumar <saikumargurajalaa@gmail.com>
2026-05-16 12:29:54 +0530

    Updated files

---
+++ classes/class obj.py
@@ -0,0 +1,11 @@
+class Fruits:
+    def orange(self):
+        print("orange")
+    def apple(self):
+        print("apple")
+    def banana(self):
+        print("banana")
+obj=Fruits()
+obj.orange()
+obj.apple()
+obj.banana()
---
+++ functions/new.py
@@ -0,0 +1,4 @@
+def student(name,age=86,marks=98):
+    print(name,"age is",age,"marks are",marks)
+student("ram",23,23)
+student("sita",36)
